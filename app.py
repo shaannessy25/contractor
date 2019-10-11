3d0be15643327a96dec83da882e8c038c038bfc4
Shaan <Shaan@admins-MacBook-Pro.local>
2019-10-10 22:54:32 -0700

    deploying to heroku

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/contractor')
 client = MongoClient(host=f'{host}?retryWrites=false')
 client = MongoClient()
 db = client.contractor
